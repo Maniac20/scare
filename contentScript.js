5cba85404f211b9c381c555623b8514e575616f5
gaurav <gauravsri736@gmail.com>
2023-08-05 15:30:04 +0530

    demo commit

--- contentScript.js
+++ contentScript.js
@@ -3,19 +3,7 @@ function getCurrentUrl() {
 
 }
 window.onload = function () {
-
-  console.log('heygfdgdfgdf')
-  
-
-  
-//  console
-  
-
-  //here we will check if the their is login error then i should reset the form and give a chance to refill
-
-  // chrome.runtime.onMessage.addListener(function (request,sender,sendResponse) {
-  //   console.log("",requ  est.action);
-  // })
+  console.log('hgf')
 
   var currenWindow = getCurrentUrl();
 
